b2b51152de36a6049ecdb93738ab8b1d72ff2c19
Christian Bedo <thierry.moa.1410@gmail.com>
2023-04-06 12:00:56 -0400

    init project with dependance

---
+++ src/main/java/com/fujitsu/affectationf/AffectationfApplication.java
@@ -0,0 +1,13 @@
+package com.fujitsu.affectationf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffectationfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffectationfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fujitsu/affectationf/AffectationfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fujitsu.affectationf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AffectationfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
